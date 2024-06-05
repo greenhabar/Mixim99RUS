abcdc9492e0adfb0c431f5d6d2b4c8c57bbc73fd
LorPilek <Vmurach89@gmail.com>
2024-06-05 19:20:23 +1000

    Trigger

---
+++ WindowsFormsApp1/Trigger.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    internal class Trigger
+    {
+        public PictureBox pic;
+        public bool active;
+        public int type;
+        public string path;
+
+        public Trigger(PictureBox pic, int type, string path)
+        {
+            this.pic = pic;
+            this.type = type;
+            this.path = path;
+        }
+
+        public void ShowEvent()
+        {
+            switch (type)
+            {
+                case 1:
+                    Console.WriteLine("Смена локации");
+                    // вызов функции ReadJson()
+                    break;
+                case 2:
+                    Console.WriteLine("Всплывает диалог");
+                    // вызов функции ReadJson()
+                    break;
+                case 3:
+                    Console.WriteLine("Всплывает картинка");
+                    // вызов функции ReadJson()
+                    break;
+            }
+        }
+
+    }
+}
